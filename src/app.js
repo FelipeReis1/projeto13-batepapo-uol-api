bb6df48839961070a50f3d6ab0ee03df69cf881b
FelipeReis1 <feliperg15@yahoo.com.br>
2023-01-16 00:54:34 -0300

    correções em POST /participants e POST /messages

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.post("/participants", async (req, res) => {
     if (!name) {
       return res.sendStatus(422);
     }
-    const nameSchema = joi.object({ name: joi.string().min(1).required() });
+    const nameSchema = joi.object({ name: joi.string().required() });
     const nameValidation = nameSchema.validate({ name });
     if (nameValidation.error) {
       const errors = nameValidation.details.map((d) => d.message);
@@ -65,9 +65,12 @@ app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
   const { user } = req.headers;
   try {
+    if (!to || !text || !type) {
+      return res.sendStatus(422);
+    }
     const messageSchema = joi.object({
-      to: joi.string().min(1).required(),
-      text: joi.string().min(1).required(),
+      to: joi.string().required(),
+      text: joi.string().required(),
       type: joi.string().valid("message", "private_message").required(),
     });
     const messageValidation = messageSchema.validate({ to, text, type });
