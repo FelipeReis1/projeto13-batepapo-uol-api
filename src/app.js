a3ea9f79c46e0fe84cc76f07ec0ee7dc5111c2db
FelipeReis1 <feliperg15@yahoo.com.br>
2023-01-15 22:06:59 -0300

    requisito POST /messages concluído

--- src/app.js
+++ src/app.js
@@ -61,4 +61,36 @@ app.get("/participants", async (req, res) => {
   res.status(200).send(participants);
 });
 
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required(),
+  });
+  const messageValidation = messageSchema.validate({ to, text, type });
+  if (!messageValidation) {
+    return res.sendStatus(422);
+  }
+  const userRegistered = await db
+    .collection("participants")
+    .findOne({ name: user });
+  if (!userRegistered) {
+    return res.sendStatus(422);
+  }
+  try {
+    await db.collection("messages").insertOne({
+      from: user,
+      to,
+      text,
+      type,
+      time: dayjs(Date.now()).format("HH:mm:ss"),
+    });
+    return res.sendStatus(201);
+  } catch {
+    return res.sendStatus(422);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on port: ${PORT} `));
